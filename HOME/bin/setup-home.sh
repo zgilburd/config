35c0989586b089ecdcfbabc8ace6092f8c462c96
Zack Gilburd <zack@gilburd.com>
2015-03-17 16:44:44 -0700

    Clone git repos properly

--- HOME/bin/setup-home.sh
+++ HOME/bin/setup-home.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 for i in `find \`pwd\` -maxdepth 1|grep -v -e LICENSE -e README.md`; do ln -sf $i ~/;done
+cd ~/git/
+git clone https://github.com/chriskempson/tomorrow-theme.git
+git clone https://github.com/tpope/vim-surround.git 
+git clone https://github.com/edsono/vim-matchit.git 
+git clone https://github.com/groenewege/vim-less.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-pathogen.git         
+
