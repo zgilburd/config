981385094cbd0e003094ed2710ec97463295f1b7
Zack Gilburd <zack@gilburd.com>
2015-09-10 15:14:55 -0700

    Overall improvements, handle files with spaces

--- HOME/bin/recryptgpg.sh
+++ HOME/bin/recryptgpg.sh
@@ -7,18 +7,33 @@
 GPG_IDS='A467FF58 E1D6480F'
 
 recrypt () {
-	tmpfile=`mktemp`
-	file=$1
-	mv $file $tmpfile
-	gpg2 -d $tmpfile | gpg2 `for i in $GPG_IDS; do echo -n "-r $i " ; done` -o $file -e
-	#mv $tmpfile "$file".bak
-	rm -f $tmpfile
+	src=`mktemp`
+	dst=`mktemp`
+
+	## I want a name of a good temporary file that's available, but I don't want 
+	## that file to be created just yet because I will test for its presence 
+	## below before making filesystem changes to the original file.
+
+	rm -f $dst
+	file="$1"
+	cp "$file" $src
+	gpg2 -d $src | gpg2 `for i in $GPG_IDS; do echo -n "-r $i " ; done` \
+		-o $dst -e
+	if [ -f $dst ]; then
+		## for testing purposes
+		cp "$file" "$file".bak
+		mv $dst "$file"
+	else
+		echo -e "\n\t** ERROR did not make changes to $file"
+	fi
+
+	rm -f $src
 }
 
-if [ -f $1 ]; then
-	echo -e "\nPWorking on $1 ...\n"
-	recrypt $1
+if [ -f "$1" ]; then
+	echo -e "\nWorking on $1 ...\n"
+	recrypt "$1"
 else 
-	echo bye
+	echo "You must specify a present file in order to recrypt it."
 fi
-	
+
