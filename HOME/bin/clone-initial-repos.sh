ab3c2a18728e2a4f0a512457b758eb5aa0f5a24c
Zack Gilburd <zack@gilburd.com>
2015-10-14 05:20:35 -0700

    Removing unused file

--- HOME/bin/clone-initial-repos.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-git clone https://github.com/chriskempson/tomorrow-theme.git
-git clone https://github.com/tpope/vim-pathogen.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/ervandew/supertab.git vim-supertab
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tmhedberg/matchit.git vim-matchit
-git clone https://github.com/groenewege/vim-less.git
