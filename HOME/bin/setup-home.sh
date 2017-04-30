beb20c761fde99fbf52555813c2abf3c405b6121
Zack Gilburd <zack@gilburd.com>
2017-04-30 02:32:01 -0700

    Adding a useful vim plugin for easy register paste

--- HOME/bin/setup-home.sh
+++ HOME/bin/setup-home.sh
@@ -11,6 +11,7 @@ https://github.com/tpope/vim-surround.git
 https://github.com/edsono/vim-matchit.git 
 https://github.com/groenewege/vim-less.git
 https://github.com/scrooloose/nerdtree.git
+https://github.com/vim-scripts/ReplaceWithRegister.git
 https://github.com/tpope/vim-fugitive.git
 https://github.com/plasticboy/vim-markdown.git
 https://github.com/majutsushi/urxvt-font-size.git
